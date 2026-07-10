57e02dcd0abdfba3404b229c0f805e2d38433797
GigatonProductions <benjaminag.2027@mtchs.org>
2026-07-09 21:42:28 -0600

    lesson5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         "id": "1",
@@ -48,6 +50,14 @@ app.delete('/api/persons/:id', (request, response) => {
   person = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = Math.floor(Math.random() * 65536)
+    console.log(person)
+    response.json(person)
+
 })
 
 app.get('/info', (request, response) => {
